f64c836ffb14497a9ab1bb87bd8eb15aa7b403f2
matthew.arrenberg@payd.co <matthew.arrenberg@payd.co>
2022-12-13 13:41:39 +0200

    Added about me page and top tv shows

---
+++ idr-imdb/src/app/top-tv-shows/top-tv-shows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-tv-shows',
+  templateUrl: './top-tv-shows.component.html',
+  styleUrls: ['./top-tv-shows.component.css']
+})
+export class TopTvShowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
